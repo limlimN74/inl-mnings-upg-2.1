788b4e2b5b0000481ec9404f3577b81c756688f1
lini07003 <lini07003@E-PW05C37N>
2024-10-09 11:58:04 +0200

    Add project files.

---
+++ inlämnings upg 2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace inlämnings_upg_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Frågar personen//
+            Console.WriteLine("Vad är din favoritbok? ");
+            string bok = Console.ReadLine(); 
+            Console.WriteLine("Din favoritbok är: " + bok + ".");
+            
+        }
+        
+    }
+}
